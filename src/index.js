435bbc29a24584152e53d338e1fe9b2383a050a3
Danik2023 <danik.kyrteu2@gmail.com>
2024-11-21 12:40:58 +0200

    ...

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+const breedSl = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  loader.classList.remove('is-hidden');
+  breedSl.classList.add('is-hidden');
+
+  
+  fetchBreeds()
+    .then(breeds => {
+      breedSl.innerHTML = breeds
+        .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+        .join('');
+      loader.classList.add('is-hidden');
+      breedSl.classList.remove('is-hidden');
+    })
+    .catch(() => {
+      error.classList.remove('is-hidden');
+      loader.classList.add('is-hidden');
+    });
+});
+
+
+import { fetchCatByBreed } from './cat-api';
+
+
+
+breedSl.addEventListener('change', event => {
+  const breedId = event.target.value;
+
+  if (!breedId) return;
+
+  catInfo.innerHTML = '';
+  loader.classList.remove('is-hidden');
+  error.classList.add('is-hidden');
+
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      const { url, breeds } = cat;
+      const { name, description, temperament } = breeds[0];
+      catInfo.innerHTML = `
+        <img src="${url}" alt="${name}" width="400">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <p><strong>Temperament:</strong> ${temperament}</p>
+      `;
+      loader.classList.add('is-hidden');
+    })
+    .catch(() => {
+      error.classList.remove('is-hidden');
+      loader.classList.add('is-hidden');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// breedSl.innerHTML =
+//  ` <option value="breed1">Breed 1</option>
+//   <option value="breed2">Breed 2</option>
+// `;
+
+// catInfo.innerHTML = `
+//   <img src="cat-image-url.jpg" alt="Cute Cat">
+//   <h2>Cat Name</h2>
+//   <p>Description of the cat</p>
+//   <p>Temperament: Playful, Friendly</p>
+// `;
